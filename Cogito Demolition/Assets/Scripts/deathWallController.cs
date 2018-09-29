ed518e2e968f3f4a99a244e838cfd7f5d4cb0350
Unknown <alberto.berenguer.97@gmail.com>
2018-09-29 20:39:08 +0200

    Menú precioso

---
+++ Cogito Demolition/Assets/Scripts/deathWallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathWallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad < 2)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
--- Cogito Demolition/Assets/scroll.cs
+++ Cogito Demolition/Assets/scroll.cs
@@ -11,7 +11,7 @@ public class scroll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 offset = new Vector2(0, -Time.time * speed);
+        Vector2 offset = new Vector2(0,-Time.time * speed);
         GetComponent<Renderer>().material.mainTextureOffset = offset;
     }
 }
