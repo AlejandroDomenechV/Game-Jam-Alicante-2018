3fcde0cfd70941e301860384d7a097b0b0204774
Alejandro Domenech Vicedo <A.DomenechVicedo@gmail.com>
2018-09-30 11:17:04 +0200

    score in win

--- Cogito Demolition/Assets/Scripts/GameWinManager.cs
+++ Cogito Demolition/Assets/Scripts/GameWinManager.cs
@@ -3,41 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class GameOverManager : MonoBehaviour {
+public class GameWinManager : MonoBehaviour {
 
-
-
-    public TextMeshProUGUI time;
     public TextMeshProUGUI score;
     public TextMeshProUGUI highScore;
     public TextMeshProUGUI platformCount;
-    public TextMeshProUGUI frase;
-
     // Use this for initialization
     void Start () {
 
-        if(GameManagerController.highScore < GameManagerController.score)
+        if (GameManagerController.highScore < GameManagerController.score)
         {
             GameManagerController.highScore = GameManagerController.score;
         }
 
-        print("La score: " + GameManagerController.score);
-        print("El tiempo: " + Time.timeSinceLevelLoad);
-        print("El piso: " + GameManagerController.platformCount);
         highScore.text = string.Format("{0}", GameManagerController.highScore);
         score.text = string.Format("{0}", GameManagerController.score);
-        time.text = string.Format("{0:00.00}", GameManagerController.time);
         platformCount.text = string.Format("{0}", GameManagerController.platformCount - 1);
 
-
     }
-
-    // Update is called once per frame
-    void Update () {
+	
+	// Update is called once per frame
+	void Update () {
 		
 	}
-
-
-
-
 }
